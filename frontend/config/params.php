144a252a06e4d31be6049726f151934d5c072a56
Vadim Frolov <fwadim@mail.ru>
2021-04-17 13:00:34 +0500

    betta

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'webmaster@vmaya.ru',
-    'senderEmail'=>	'webmaster@vmaya.ru',
+    'adminEmail' => 'webmaster@dcook.site',
+    'senderEmail'=>	'webmaster@dcook.site',
     'geocodingKey' => 'AIzaSyCoKk5jGpU844xvp1--OmnPaF7CvA2XlxY',
     'mapApiKey' => 'AIzaSyBzErLfg0nBPSCmP2LcYq0Y5A-C0GIuBMM',
     'countPerPage' => 10
